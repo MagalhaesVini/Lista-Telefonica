1d0fbc97eaeb454158985db1b49f2c8f92ce5ec6
Vinicius Magalhaes <vinizipi@gmail.com>
2024-03-14 19:20:43 -0300

    mascara id

--- app/components/FormularioEdicao.jsx
+++ app/components/FormularioEdicao.jsx
@@ -4,16 +4,35 @@ import TextField from '@mui/material/TextField';
 import SendIcon from '@mui/icons-material/Send';
 
 function FormularioEdicao({ itemEditado, onCancel, onSave }) {
+  const formatarDocumento = (documento) => {
+    if (!documento) return '';
+    const cleaned = ('' + documento).replace(/\D/g, '');
+    if (cleaned.length === 11) {
+      return cleaned.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+    } else if (cleaned.length === 14) {
+      return cleaned.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, '$1.$2.$3/$4-$5');
+    }
+    return documento;
+  };
+
+  const limparMascara = (documento) => {
+    return documento.replace(/[^\d]+/g, '');
+  };
+
   const [formData, setFormData] = useState({
     nome: itemEditado ? itemEditado.nome : '',
     celular: itemEditado ? itemEditado.celular : '',
-    documento_identificacao: itemEditado ? itemEditado.documento_identificacao : '',
+    documento_identificacao: itemEditado ? formatarDocumento(itemEditado.documento_identificacao) : '',
     endereço: itemEditado ? itemEditado.endereço : '',
     empresa: itemEditado ? itemEditado.empresa : '',
     setor: itemEditado ? itemEditado.setor : '',
     comercial: itemEditado ? itemEditado.comercial : '',
     outros: itemEditado ? itemEditado.outros : '',
   });
+
+  const handleInputChange = (fieldName, value) => {
+    setFormData({ ...formData, [fieldName]: formatarDocumento(value) });
+  };
 
   const handleSave = async () => {
     try {
@@ -22,7 +41,10 @@ function FormularioEdicao({ itemEditado, onCancel, onSave }) {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(formData),
+        body: JSON.stringify({
+          ...formData,
+          documento_identificacao: limparMascara(formData.documento_identificacao) // Removendo a máscara
+        }),
       });
 
       if (!response.ok) {
@@ -40,13 +62,13 @@ function FormularioEdicao({ itemEditado, onCancel, onSave }) {
     <div style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', backgroundColor: 'rgba(255, 255, 255, 0.9)', padding: '20px', borderRadius: '50px', boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)', zIndex: 9999 }}>
       <p style={{ marginBottom: '4px', fontSize: '1rem', fontWeight: 'bold', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>Editar Dados</p>
       <TextField label="Nome" value={formData.nome} onChange={(e) => setFormData({ ...formData, nome: e.target.value })} fullWidth style={{ marginBottom: '5px' }} />
-      <TextField label="Celular" value={formData.celular} onChange={(e) => setFormData({ ...formData, celular: e.target.value })} fullWidth style={{ marginBottom: '5px' }} />
-      <TextField label="Documento de Identificação" value={formData.documento_identificacao} onChange={(e) => setFormData({ ...formData, documento_identificacao: e.target.value })} fullWidth style={{ marginBottom: '5px' }} />
+      <TextField label="Celular" value={formData.celular} onChange={(e) => setFormData({ ...formData, celular: e.target.value })} fullWidth inputProps={{ maxLength: 11 }} style={{ marginBottom: '5px' }} />
+      <TextField label="Documento de Identificação" value={formData.documento_identificacao} onChange={(e) => handleInputChange('documento_identificacao', e.target.value)} fullWidth inputProps={{ maxLength: 18 }} style={{ marginBottom: '5px' }} />
       <TextField label="Endereço" value={formData.endereço} onChange={(e) => setFormData({ ...formData, endereço: e.target.value })} fullWidth style={{ marginBottom: '5px' }} />
       <TextField label="Empresa" value={formData.empresa} onChange={(e) => setFormData({ ...formData, empresa: e.target.value })} fullWidth style={{ marginBottom: '5px' }} />
       <TextField label="Setor" value={formData.setor} onChange={(e) => setFormData({ ...formData, setor: e.target.value })} fullWidth style={{ marginBottom: '5px' }} />
-      <TextField label="Telefone Comercial" value={formData.comercial} onChange={(e) => setFormData({ ...formData, comercial: e.target.value })} fullWidth style={{ marginBottom: '5px' }} />
-      <TextField label="Outros" value={formData.outros} onChange={(e) => setFormData({ ...formData, outros: e.target.value })} fullWidth style={{ marginBottom: '5px' }} />
+      <TextField label="Telefone Comercial" value={formData.comercial} onChange={(e) => setFormData({ ...formData, comercial: e.target.value })} fullWidth inputProps={{ maxLength: 10 }} style={{ marginBottom: '5px' }} />
+      <TextField label="Outros" value={formData.outros} onChange={(e) => setFormData({ ...formData, outros: e.target.value })} fullWidth inputProps={{ maxLength: 11 }} style={{ marginBottom: '5px' }} />
       <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '10px' }}>
         <Button variant="outlined" onClick={onCancel}>Cancelar</Button>
         <Button variant="contained" color="primary" startIcon={<SendIcon />} onClick={handleSave}>Salvar</Button>
